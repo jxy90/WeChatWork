b0199e8db77e5a92175d82f5bd5cd5673d3f077b
SS-CN-JIANG.XIOAYU <>
2019-11-25 10:21:33 +0800

    refactor code

--- send_message/dto.go
+++ send_message/dto.go
@@ -1,32 +1,59 @@
 package send_message
 
+const (
+	MsgType_Text     = "text"
+	MsgType_Image    = "image"
+	MsgType_Voice    = "voice"
+	MsgType_Video    = "video"
+	MsgType_File     = "file"
+	MsgType_TextCard = "textcard"
+	MsgType_News     = "news"
+	MsgType_MPNews   = "mpnews"
+	MsgType_MarkDown = "markdown"
+	//MsgType_MiniProgramNotice = "miniprogram_notice"
+	MsgType_TaskCard = "taskcard"
+)
+
 type WeChatWorkRequest struct {
-	AccessToken        string             `json:"access_token"`
-	ToUser             string             `json:"touser"`
-	ToParty            string             `json:"toparty"`
-	ToTag              string             `json:"totag"`
-	MessageType        string             `json:"type"`
-	AgentId            string             `json:"agentid"`
-	Safe               string             `json:"safe"`
-	EnableIdTrans      string             `json:"enable_id_trans"`
-	WeChatWorkText     WeChatWorkText     `json:"text"`
-	WeChatWorkTextCard WeChatWorkTextCard `json:"textcard"`
-	WeChatWorkMarkDown WeChatWorkMarkDown `json:"markdown"`
+	AccessToken   string             `json:"access_token"`
+	ToUser        string             `json:"touser"`
+	ToParty       string             `json:"toparty"`
+	ToTag         string             `json:"totag"`
+	MessageType   string             `json:"msgtype"`
+	AgentId       int                `json:"agentid"`
+	Safe          int                `json:"safe"`
+	EnableIdTrans int                `json:"enable_id_trans"`
+	Text          WeChatWorkText     `json:"text"`
+	Image         WeChatWorkImage    `json:"image"`
+	Voice         WeChatWorkVoice    `json:"voice"`
+	Video         WeChatWorkVideo    `json:"video"`
+	File          WeChatWorkFile     `json:"file"`
+	TextCard      WeChatWorkTextCard `json:"textcard"`
+	News          WeChatWorkNews     `json:"news"`
+	MPNews        WeChatWorkMPNews   `json:"mpnews"`
+	MarkDown      WeChatWorkMarkDown `json:"markdown"`
+	//MiniProgramNotice WeChatWorkMiniProgramNotice `json:"miniprogram_notice"`
+	TaskCard WeChatWorkTaskCard `json:"taskcard"`
 }
+
 type WeChatWorkText struct {
 	Content string `json:"content"`
 }
+
 type WeChatWorkImage struct {
 	MediaId string `json:"media_id"`
 }
+
 type WeChatWorkVoice struct {
 	MediaId string `json:"media_id"`
 }
+
 type WeChatWorkVideo struct {
 	MediaId     string `json:"media_id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 }
+
 type WeChatWorkFile struct {
 	MediaId string `json:"media_id"`
 }
@@ -45,7 +72,7 @@ type WeChatWorkNewsArticle struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Url         string `json:"url"`
-	PicUrl      string `json:"picUrl"`
+	PicUrl      string `json:"picurl"`
 }
 
 type WeChatWorkMPNews struct {
@@ -62,6 +89,35 @@ type WeChatWorkMPNewsArticle struct {
 
 type WeChatWorkMarkDown struct {
 	Content string `json:"content" `
+}
+
+type WeChatWorkMiniProgramNotice struct {
+	AppId             string                            `json:"appid" `
+	Page              string                            `json:"page" `
+	Title             string                            `json:"title" `
+	Description       string                            `json:"description" `
+	EmphasisFirstItem bool                              `json:"emphasis_first_item" `
+	ContentItem       []WeChatWorkMiniProgramNoticeItem `json:"content_item" `
+}
+
+type WeChatWorkMiniProgramNoticeItem struct {
+	Key   string `json:"key" `
+	Value string `json:"value" `
+}
+
+type WeChatWorkTaskCard struct {
+	title       string `json:"title" `
+	description string `json:"description" `
+	url         string `json:"url" `
+	task_id     string `json:"task_id" `
+	btn         string `json:"btn" `
+}
+type WeChatWorkTaskCardBtn struct {
+	Key         string `json:"key" `
+	Name        string `json:"name" `
+	ReplaceName string `json:"replace_name" `
+	Color       string `json:"color" `
+	IsBold      bool   `json:"is_bold" `
 }
 
 var WeChatWorkResponse struct {
--- send_message/send_mesage.go
+++ send_message/send_mesage.go
@@ -8,42 +8,33 @@ import (
 	"net/http"
 )
 
-const (
-	WeChatMsgType_Text     = "text" //文本
-	WeChatMsgType_Image    = "image"
-	WeChatMsgType_Voice    = "voice"
-	WeChatMsgType_Video    = "video"
-	WeChatMsgType_File     = "file"
-	WeChatMsgType_TextCatd = "textcard" //文本卡片
-	WeChatMsgType_MarkDown = "markdown" //markdown消息
-)
-
 //https://work.weixin.qq.com/api/doc#90000/90135/90236
 func Send(request WeChatWorkRequest) error {
-	senMsgUrl := WeChatWorkURL + "/send?access_token=" + request.AccessToken
-	msg := make(map[string]interface{}, 0)
-	detail := make(map[string]interface{}, 0)
-	msg["touser"] = request.ToUser
-	msg["toparty"] = request.ToParty
-	msg["totag"] = request.ToParty
-	msg["msgtype"] = request.MessageType
-	msg["agentid"] = request.AgentId
-	msg["safe"] = 0
-	msg["enable_id_trans"] = 0
-
 	switch request.MessageType {
-	case WeChatMsgType_Text:
-		detail = makeDetailMsgOfText(request.WeChatWorkText)
-	case WeChatMsgType_TextCatd:
-		detail = makeDetailMsgOfTextCard(request.WeChatWorkTextCard)
-	case WeChatMsgType_MarkDown:
-		detail = makeDetailMsgOfMarkDown(request.WeChatWorkMarkDown)
+	case MsgType_Text,
+		MsgType_Image,
+		MsgType_Voice,
+		MsgType_Video,
+		MsgType_File,
+		MsgType_TextCard,
+		MsgType_News,
+		MsgType_MPNews,
+		MsgType_MarkDown,
+		MsgType_TaskCard:
 	default:
-		return errors.New("miss MsgType")
+		errors.New("invalid MessageType:" + request.MessageType)
 	}
-
-	msg[request.MessageType] = detail
-	_, err := httpreq.New(http.MethodPost, senMsgUrl, msg).
+	senMsgUrl := WeChatWorkURL + "/send?access_token=" + request.AccessToken
+	requestBytes, err := json.Marshal(request)
+	if err != nil {
+		return errors.New("struct Marshal err")
+	}
+	requestMap := make(map[string]interface{}, 0)
+	err = json.Unmarshal(requestBytes, &requestMap)
+	if err != nil {
+		return errors.New("struct Unmarshal err")
+	}
+	_, err = httpreq.New(http.MethodPost, senMsgUrl, requestMap).
 		WithBehaviorLogContext(behaviorlog.FromCtx(nil)).
 		Call(&WeChatWorkResponse)
 	if err != nil {
@@ -54,25 +45,4 @@ func Send(request WeChatWorkRequest) error {
 	}
 	errmsg, _ := json.Marshal(WeChatWorkResponse)
 	return errors.New("send WeChatWork message error. " + string(errmsg))
-}
-
-func makeDetailMsgOfText(textCard WeChatWorkText) map[string]interface{} {
-	detail := make(map[string]interface{}, 0)
-	detail["content"] = textCard.Content
-	return detail
-}
-
-func makeDetailMsgOfTextCard(textCard WeChatWorkTextCard) map[string]interface{} {
-	detail := make(map[string]interface{}, 0)
-	detail["title"] = textCard.Title
-	detail["description"] = textCard.Description
-	detail["url"] = textCard.Url
-	detail["btntxt"] = textCard.Btntxt
-	return detail
-}
-
-func makeDetailMsgOfMarkDown(markDown WeChatWorkMarkDown) map[string]interface{} {
-	detail := make(map[string]interface{}, 0)
-	detail["content"] = markDown.Content
-	return detail
 }
